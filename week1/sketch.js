bd3f67dc7f20991255c7e4122efcdaec62613668
fylzzz <ichocolqtemilk@outlook.com>
2026-03-03 10:51:33 +1000

    try model loading

--- week1/sketch.js
+++ week1/sketch.js
@@ -1,7 +1,32 @@
+let object;
+
+function preload() {
+    //object = loadModel('/assets/Lightning.obj', true, handleModel, handleError);
+    fetch('/assets/Lightning.obj')
+        .then(r => r.text())
+        .then(text => {
+            console.log('File size:', text.length, 'chars');
+            console.log('First 200 chars:', text.substring(0, 200));
+        });
+}
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(800, 600, WEBGL);
 }
 
 function draw() {
+    if(!object) return;
+
     background(220);
+
+    model(object);
+}
+
+function handleModel(data) {
+    object = data;
+    console.log(object.gid);
+}
+
+function handleError(error) {
+    console.error(error);
 }
