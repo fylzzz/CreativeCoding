325c17df6f8a7034f25fc78fee6f110cce4a6a05
fylzzz <ichocolqtemilk@outlook.com>
2026-03-17 09:14:33 +1000

    make q1 & 2 worse

--- Assignment1/q2/sketch.js
+++ Assignment1/q2/sketch.js
@@ -1,20 +1,23 @@
-let circles = [];
+//let circles = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    background(255);
 }
 
 function draw() {
-    background(255);
+    //background(255);
 
-    circles.forEach(c => c.render());
+    //circles.forEach(c => c.render());
 }
 
 function mouseClicked() {
-    circles.push(new Circle(mouseX, mouseY))
+    //circles.push(new Circle(mouseX, mouseY))
+    fill(random(255), random(255), random(255));
+    circle(mouseX, mouseY, random(20,100));
 }
 
-class Circle {
+/*class Circle {
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -26,4 +29,4 @@ class Circle {
         fill(this.c);
         circle(this.x, this.y, this.d);
     }
-}
+}*/
