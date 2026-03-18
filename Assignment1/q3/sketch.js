8b54b85ab769feeaa27324dbf5658e0af282ff01
fylzzz <139827535+fylzzz@users.noreply.github.com>
2026-03-19 08:44:05 +1000

    mostly finish q3

--- Assignment1/q3/sketch.js
+++ Assignment1/q3/sketch.js
@@ -28,11 +28,59 @@ function draw() {
 
 function drawGraph() {
     graphHeight = windowHeight-padding*2;
+
     strokeWeight(5);
     line(padding, padding, padding, windowHeight-padding);
     line(padding, windowHeight-padding, windowWidth-padding, windowHeight-padding);
+
+    strokeWeight(10);
+    textAlign(LEFT, CENTER);
+    text((largest/5*1), padding-20, padding + graphHeight/5*4);
+    text((largest/5*2), padding-20, padding + graphHeight/5*3);
+    text((largest/5*3), padding-20, padding + graphHeight/5*2);
+    text((largest/5*4), padding-20, padding + graphHeight/5*1);
+    text((largest/5*5), padding-20, padding + graphHeight/5*0);
+
     strokeWeight(2);
-    for(i = 0; i <= 5; i++) {
-        line(padding, padding + graphHeight/5*i, windowWidth-padding, padding + graphHeight/5*i);
-    }
+    line(padding, padding + graphHeight/5*0, windowWidth-padding, padding + graphHeight/5*0);
+    line(padding, padding + graphHeight/5*1, windowWidth-padding, padding + graphHeight/5*1);
+    line(padding, padding + graphHeight/5*2, windowWidth-padding, padding + graphHeight/5*2);
+    line(padding, padding + graphHeight/5*3, windowWidth-padding, padding + graphHeight/5*3);
+    line(padding, padding + graphHeight/5*4, windowWidth-padding, padding + graphHeight/5*4);
+
+    comedyBar();
+    actionBar();
+    romanceBar();
+    dramaBar();
+    scifiBar();
+}
+
+function comedyBar() {
+    fill(255, 0, 0);
+    rectMode(CORNERS);
+    rect(padding+10, windowHeight-padding, padding+110, (windowHeight - padding - graphHeight/largest*comedy));
+}
+
+function actionBar() {
+    fill(0, 255, 0);
+    rectMode(CORNERS);
+    rect(padding+120, windowHeight-padding, padding+220, (windowHeight - padding - graphHeight/largest*action));
+}
+
+function romanceBar() {
+    fill(0, 0, 255);
+    rectMode(CORNERS);
+    rect(padding+230, windowHeight-padding, padding+330, (windowHeight - padding - graphHeight/largest*romance));
+}
+
+function dramaBar() {
+    fill(255, 255, 0);
+    rectMode(CORNERS);
+    rect(padding+340, windowHeight-padding, padding+440, (windowHeight - padding - graphHeight/largest*drama));
+}
+
+function scifiBar() {
+    fill(255, 0, 255);
+    rectMode(CORNERS);
+    rect(padding+450, windowHeight-padding, padding+550, (windowHeight - padding - graphHeight/largest*scifi));
 }
