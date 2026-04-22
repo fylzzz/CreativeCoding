4ed7b4728350d119bf8fc226966c5abde28b3820
Will Bartlett <139827535+fylzzz@users.noreply.github.com>
2026-04-23 08:33:48 +1000

    finish a2q1

--- Assignment2/mazegame/sketch.js
+++ Assignment2/mazegame/sketch.js
@@ -1,16 +1,25 @@
 let tileArray = [];
 let TILE_SIZE;
+let startX;
+let startY
+let player;
+let walls
+let endTile
 
 function preload() {
     mapData = loadStrings('assets/maze.txt');
 
     floorSprite = loadImage('assets/grass.png');
     wallSprite = loadImage('assets/cobble.png');
+    startSprite = loadImage('assets/planks.png');
+    endSprite = loadImage('assets/diamond.png');
+    playerSprite = loadImage('assets/player.png');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     tileArray = mapData.map(line => line.split(''));
+    walls = new Group();
 
     //ensure map fits to screen
     if (tileArray.length > tileArray[0].length) {
@@ -19,6 +28,7 @@ function setup() {
         TILE_SIZE = windowWidth/tileArray[0].length;
     }
 
+    //iterate through tile map and set sprites
     for (let row = 0; row < tileArray.length; row++) {
         for (let col = 0; col < tileArray[row].length; col++) {
             let char = tileArray[row][col];
@@ -32,39 +42,70 @@ function setup() {
                 case '1':
                 sprite.addImage(wallSprite);
                 sprite.scale = TILE_SIZE / wallSprite.width;
+                sprite.width = TILE_SIZE;
+                sprite.height = TILE_SIZE;
+                sprite.immovable = true;
+                walls.add(sprite);
                 break;
                 case '2':
-                fill(0, 0, 255);
+                sprite.addImage(startSprite);
+                sprite.scale = TILE_SIZE / startSprite.width;
+                startX = col * TILE_SIZE + TILE_SIZE / 2;
+                startY = row * TILE_SIZE + TILE_SIZE / 2;
                 break;
                 case '3':
-                fill(0, 0, 0);
+                sprite.addImage(endSprite);
+                sprite.scale = TILE_SIZE / endSprite.width;
+                endTile = sprite;
                 break;
             }
         }
     }
+
+    //setup player sprite
+    player = createSprite(startX, startY, TILE_SIZE, TILE_SIZE);
+    player.addImage(playerSprite);
+    player.scale = TILE_SIZE / playerSprite.width;
+    player.width = TILE_SIZE * 0.7;
+    player.height = TILE_SIZE * 0.7;
+    player.bounciness = 1;
 }
 
 function draw() {
     background(220);
     
-    /*for (let row = 0; row < tileArray.length; row++) {
-        for (let col = 0; col < tileArray[row].length; col++) {
-            switch (tileArray[row][col]) {
-                case '0':
-                fill(255, 0, 0);
-                break;
-                case '1':
-                fill(0, 255, 0);
-                break;
-                case '2':
-                fill(0, 0, 255);
-                break;
-                case '3':
-                fill(0, 0, 0);
-                break;
-            }
-            rect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-        }
-    }*/
-   drawSprites();
+    //wait until player is ready
+    if(!player) return;
+
+    //handle user input
+    if(keyIsDown(87)) {
+        player.velocity.y = -3;
+        player.rotation = 90;
+    }
+    if(keyIsDown(83)) {
+        player.velocity.y = 3;
+        player.rotation = 270;
+    }
+    if(keyIsDown(65)) {
+        player.velocity.x = -3;
+        player.rotation = 0;
+    }
+    if(keyIsDown(68)) {
+        player.velocity.x = 3;
+        player.rotation = 180; 
+    }
+
+    if (!keyIsDown(87) && !keyIsDown(83)) player.velocity.y = 0;
+    if (!keyIsDown(65) && !keyIsDown(68)) player.velocity.x = 0;
+
+    //check for collisions with walls and bounce off
+    player.bounce(walls);
+
+    //check if player has reached goal and reset
+    if(player.overlap(endTile)) {
+        player.position.x = startX;
+        player.position.y = startY; 
+    }
+
+    drawSprites();
 }
